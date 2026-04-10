c07dcbfda216752544fe4d00ac16c60090d2ad52
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-04-10 22:49:39 +0300

    Deleted UserService

--- obj/Debug/net10.0/WebServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56474b167d6b90be34f34bf75fa3788bc3ff59c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0830a2f6cd7fc25424edd638aea18524b036fec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
