d6d69d904ac1893d70f80532dd5559d8c6da86f7
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-04-11 13:57:52 +0300

    created Dockerfile

--- obj/Debug/net10.0/WebServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07dcbfda216752544fe4d00ac16c60090d2ad52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d52256f89419d38a5ae135f40d0b3bff10ac570")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
