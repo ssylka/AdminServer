6d52256f89419d38a5ae135f40d0b3bff10ac570
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-04-11 13:17:37 +0300

    switch to PostgreSQL

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
+//using Microsoft.EntityFrameworkCore.SqlServer;
+using WebServer.Data;
+//using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace WebServer
 {
@@ -12,8 +14,29 @@ namespace WebServer
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<Data.ApplicationDBContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("WebServer")));
+            var conn = builder.Configuration.GetConnectionString("Default");
+
+            if (string.IsNullOrEmpty(conn))
+            {
+                conn = "postgresql://postgre:tLZCpu7ssG8IQnok41aBGd37mA6uIVcD@dpg-d7cljnvlk1mc73ftr2ag-a.oregon-postgres.render.com/postgre_loth";
+            }
+
+            var uri = new Uri(conn);
+            var port = uri.Port > 0 ? uri.Port : 5432;
+
+            var userInfo = uri.UserInfo.Split(':');
+
+            var connectionString =
+                $"Host={uri.Host};" +
+                $"Port={port};" +
+                $"Database={uri.AbsolutePath.Trim('/')};" +
+                $"Username={userInfo[0]};" +
+                $"Password={userInfo[1]};" +
+                $"SSL Mode=Require;Trust Server Certificate=true";
+
+
+            builder.Services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseNpgsql(connectionString));
 
             builder.Services.AddAuthentication("Cookies")
                 .AddCookie("Cookies", options =>
@@ -22,6 +45,9 @@ namespace WebServer
                 });
 
             builder.Services.AddAuthorization();
+
+            //builder.Services.AddDbContext<ApplicationDBContext>(options =>
+            //options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
             var app = builder.Build();
 
@@ -44,6 +70,12 @@ namespace WebServer
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDBContext>();
+                db.Database.Migrate();
+            }
 
             app.Run();
         }
