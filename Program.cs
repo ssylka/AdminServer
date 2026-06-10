9af663a6372f8d45c4b83dc7144b179b8dd55ec0
Stanislav Peresypkin <75629286+ssylka@users.noreply.github.com>
2026-06-10 19:31:17 +0300

    fix db conn string

--- Program.cs
+++ Program.cs
@@ -13,27 +13,23 @@ namespace WebServer
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            var conn = builder.Configuration.GetConnectionString("Default");
-
-            if (string.IsNullOrEmpty(conn))
-            {
-                conn = "postgresql://postgre:tLZCpu7ssG8IQnok41aBGd37mA6uIVcD@dpg-d7cljnvlk1mc73ftr2ag-a.oregon-postgres.render.com/postgre_loth";
-            }
+            var conn = builder.Configuration.GetConnectionString("Default")
+    ?? "postgresql://postgre:RU61uyXeOEMcwHmB2isCA0C3GNcY94zg@dpg-d80pug3eo5us73fp870g-a/inventory_p6z2";
 
             var uri = new Uri(conn);
             var port = uri.Port > 0 ? uri.Port : 5432;
-
             var userInfo = uri.UserInfo.Split(':');
-
+            var isInternal = !uri.Host.Contains(".");
+            var sslPart = isInternal ? "SSL Mode=Disable" : "SSL Mode=Require;Trust Server Certificate=true";
+            
             var connectionString =
                 $"Host={uri.Host};" +
                 $"Port={port};" +
                 $"Database={uri.AbsolutePath.Trim('/')};" +
                 $"Username={userInfo[0]};" +
                 $"Password={userInfo[1]};" +
-                $"SSL Mode=Require;Trust Server Certificate=true";
-
-
+                sslPart;
+            
             builder.Services.AddDbContext<ApplicationDBContext>(options =>
                 options.UseNpgsql(connectionString));
 
